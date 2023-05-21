22d81c23edebc9b3fd67f893efee0aee887bd512
Abhishek <abhishekchouhannk@gmail.com>
2023-05-21 16:06:10 -0700

    added a function setupGrid to programmatically add cards to the game div

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
+function setupGrid() {
+  var cardData = [
+    { id: 'img1', frontFace: '001.png' },
+    { id: 'img2', frontFace: '002.png' },
+    { id: 'img3', frontFace: '003.png' },
+    { id: 'img4', frontFace: '001.png' },
+    { id: 'img5', frontFace: '002.png' },
+    { id: 'img6', frontFace: '003.png' }
+  ];
 
+  var gameGrid = $('#game_grid');
 
+  for (var i = 0; i < cardData.length; i++) {
+    var card = $('<div>').addClass('card');
+    var frontFace = $('<img>').attr('id', cardData[i].id).addClass('front_face').attr('src', cardData[i].frontFace).attr('alt', '');
+    var backFace = $('<img>').addClass('back_face').attr('src', 'back.webp').attr('alt', '');
+
+    card.append(frontFace);
+    card.append(backFace);
+    gameGrid.append(card);
+  }
+}
 
 const setup = () => {
+  setupGrid();
   let firstCard = undefined
   let secondCard = undefined
   $(".card").on(("click"), function () {
