0fca7ae683dbf0c34bdcf23f12090a529ec0dcc1
Abhishek <abhishekchouhannk@gmail.com>
2023-05-21 18:04:45 -0700

    cardData succesfully constructable by using constructCardData function

--- index.js
+++ index.js
@@ -1,32 +1,36 @@
 function setupGrid() {
-// Example usage: Constructing an array of card data with Pokémon URLs
-constructCardData(6)
-  .then((cardData) => {
-    console.log(cardData);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
-  var cardData = [
-    { id: 'img1', frontFace: '001.png' },
-    { id: 'img2', frontFace: '002.png' },
-    { id: 'img3', frontFace: '003.png' },
-    { id: 'img4', frontFace: '001.png' },
-    { id: 'img5', frontFace: '002.png' },
-    { id: 'img6', frontFace: '003.png' }
-  ];
+  var cardData = [];
+  // Example usage: Constructing an array of card data with Pokémon URLs
+  constructCardData(6)
+    .then((imageData) => {
+      cardData = imageData;
+      console.log(cardData);
+      // Call a function or perform any other action that relies on imageData here
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-  // var gameGrid = $('#game_grid');
 
-  // for (var i = 0; i < cardData.length; i++) {
-  //   var card = $('<div>').addClass('card');
-  //   var frontFace = $('<img>').attr('id', cardData[i].id).addClass('front_face').attr('src', cardData[i].frontFace).attr('alt', '');
-  //   var backFace = $('<img>').addClass('back_face').attr('src', 'back.webp').attr('alt', '');
+  // var imageData = [
+  //   { id: "img1", frontFace: "001.png" },
+  //   { id: "img2", frontFace: "002.png" },
+  //   { id: "img3", frontFace: "003.png" },
+  //   { id: "img4", frontFace: "001.png" },
+  //   { id: "img5", frontFace: "002.png" },
+  //   { id: "img6", frontFace: "003.png" },
+  // ];
+  var gameGrid = $('#game_grid');
 
-  //   card.append(frontFace);
-  //   card.append(backFace);
-  //   gameGrid.append(card);
-  // }
+  for (var i = 0; i < cardData.length; i++) {
+    var card = $('<div>').addClass('card');
+    var frontFace = $('<img>').attr('id', cardData[i].id).addClass('front_face').attr('src', cardData[i].frontFace).attr('alt', '');
+    var backFace = $('<img>').addClass('back_face').attr('src', 'back.webp').attr('alt', '');
+
+    card.append(frontFace);
+    card.append(backFace);
+    gameGrid.append(card);
+  }
 }
 
 const fetchPokemonInfo = async (numberOfPokemon) => {
@@ -39,7 +43,9 @@ const fetchPokemonInfo = async (numberOfPokemon) => {
   const fetchPromises = Array.from({ length: numberOfPokemon }, () =>
     fetch(`https://pokeapi.co/api/v2/pokemon/${getRandomNumber()}`)
       .then((response) => response.json())
-      .then((data) => pokemonArray.push(data.sprites.other["official-artwork"].front_default))
+      .then((data) =>
+        pokemonArray.push(data.sprites.other["official-artwork"].front_default)
+      )
   );
 
   // Wait for all promises to resolve
@@ -48,7 +54,9 @@ const fetchPokemonInfo = async (numberOfPokemon) => {
   const endTime = new Date();
   const totalTime = endTime - startTime;
 
-  console.log(`Fetched ${numberOfPokemon} Pokémon in ${totalTime} milliseconds.`);
+  console.log(
+    `Fetched ${numberOfPokemon} Pokémon in ${totalTime} milliseconds.`
+  );
 
   return pokemonArray;
 };
@@ -70,33 +78,28 @@ const constructCardData = async (numberOfCards) => {
 
 const setup = () => {
   setupGrid();
-  let firstCard = undefined
-  let secondCard = undefined
-  $(".card").on(("click"), function () {
+  let firstCard = undefined;
+  let secondCard = undefined;
+  $(".card").on("click", function () {
     $(this).toggleClass("flip");
 
-    if (!firstCard)
-      firstCard = $(this).find(".front_face")[0]
+    if (!firstCard) firstCard = $(this).find(".front_face")[0];
     else {
-      secondCard = $(this).find(".front_face")[0]
+      secondCard = $(this).find(".front_face")[0];
       console.log(firstCard, secondCard);
-      if (
-        firstCard.src
-        ==
-        secondCard.src
-      ) {
-        console.log("match")
-        $(`#${firstCard.id}`).parent().off("click")
-        $(`#${secondCard.id}`).parent().off("click")
+      if (firstCard.src == secondCard.src) {
+        console.log("match");
+        $(`#${firstCard.id}`).parent().off("click");
+        $(`#${secondCard.id}`).parent().off("click");
       } else {
-        console.log("no match")
+        console.log("no match");
         setTimeout(() => {
-          $(`#${firstCard.id}`).parent().toggleClass("flip")
-          $(`#${secondCard.id}`).parent().toggleClass("flip")
-        }, 1000)
+          $(`#${firstCard.id}`).parent().toggleClass("flip");
+          $(`#${secondCard.id}`).parent().toggleClass("flip");
+        }, 1000);
       }
     }
   });
-}
+};
 
-$(document).ready(setup)
+$(document).ready(setup);
