6f5e37687eefb9098a99159ee1f6bc1d38a1c14d
Abhishek <abhishekchouhannk@gmail.com>
2023-05-21 17:32:59 -0700

    added a function to fetch pokemon info concurrently: doing it using promises ensures pokemons are fetched quickly and stored in an array

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 function setupGrid() {
+// Example usage: Fetching information for 6 Pokémon
+fetchPokemonInfo(18)
+.then((pokemonArray) => {
+  console.log(pokemonArray);
+})
+.catch((error) => {
+  console.log(error);
+});
   var cardData = [
     { id: 'img1', frontFace: '001.png' },
     { id: 'img2', frontFace: '002.png' },
@@ -8,18 +16,42 @@ function setupGrid() {
     { id: 'img6', frontFace: '003.png' }
   ];
 
-  var gameGrid = $('#game_grid');
+  // var gameGrid = $('#game_grid');
 
-  for (var i = 0; i < cardData.length; i++) {
-    var card = $('<div>').addClass('card');
-    var frontFace = $('<img>').attr('id', cardData[i].id).addClass('front_face').attr('src', cardData[i].frontFace).attr('alt', '');
-    var backFace = $('<img>').addClass('back_face').attr('src', 'back.webp').attr('alt', '');
+  // for (var i = 0; i < cardData.length; i++) {
+  //   var card = $('<div>').addClass('card');
+  //   var frontFace = $('<img>').attr('id', cardData[i].id).addClass('front_face').attr('src', cardData[i].frontFace).attr('alt', '');
+  //   var backFace = $('<img>').addClass('back_face').attr('src', 'back.webp').attr('alt', '');
 
-    card.append(frontFace);
-    card.append(backFace);
-    gameGrid.append(card);
-  }
+  //   card.append(frontFace);
+  //   card.append(backFace);
+  //   gameGrid.append(card);
+  // }
 }
+
+const fetchPokemonInfo = async (numberOfPokemon) => {
+  const pokemonArray = [];
+  const startTime = new Date();
+
+  const getRandomNumber = () => Math.floor(Math.random() * 1000) + 1;
+
+  // Create an array of promises for fetching each Pokemon
+  const fetchPromises = Array.from({ length: numberOfPokemon }, () =>
+    fetch(`https://pokeapi.co/api/v2/pokemon/${getRandomNumber()}`)
+      .then((response) => response.json())
+      .then((data) => pokemonArray.push(data.sprites.other["official-artwork"].front_default))
+  );
+
+  // Wait for all promises to resolve
+  await Promise.all(fetchPromises);
+
+  const endTime = new Date();
+  const totalTime = endTime - startTime;
+
+  console.log(`Fetched ${numberOfPokemon} Pokémon in ${totalTime} milliseconds.`);
+
+  return pokemonArray;
+};
 
 const setup = () => {
   setupGrid();
